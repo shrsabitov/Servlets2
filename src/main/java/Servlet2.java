8d21ec2a1f4a70dad1fe71dbad7b7b4108cb521c
shrsabitov <32696262+shrsabitov@users.noreply.github.com>
2020-02-18 19:15:35 +0300

    Update Servlet2.java

--- src/main/java/Servlet2.java
+++ src/main/java/Servlet2.java
@@ -11,6 +11,9 @@ public class Servlet2 extends HttpServlet {
 
     }
 
+    //задача - вытащить параметр из request
+    //убедиться, что без обработки NumberFormatException сервлет нормально не запускается, "вылетает"
+    //поработать с указанием параметра прямо в адресной строке: http://localhost:8080/Servlet2?numberName=3 и т.д.
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String paramName = request.getParameter(UsefulNotes.numberName);
         try {
