d5c175a5d02e22c30ab155e6b3b0f4f8b68d90a5
Shamil <sh.r.sabitov@gmail.com>
2020-02-18 19:48:50 +0300

    1

--- src/main/java/basix/Servlet4.java
+++ src/main/java/basix/Servlet4.java
@@ -1,3 +1,5 @@
+package basix;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -6,13 +8,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/Servlet3")
-public class Servlet3 extends HttpServlet {
+//простейший пример, вывести 10 раз "Hello, World!" через response
+
+@WebServlet("/basix/servlet5")
+public class Servlet4 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //int number = Integer.parseInt(req.getParameter("number"));
         PrintWriter out = response.getWriter();
         for (int i = 0; i < 10; i++) {
             out.println("Hello, world!");
