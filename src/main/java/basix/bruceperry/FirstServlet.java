c3021c87974eaa1e7946f3191a03324d8e3e1716
Shamil <sh.r.sabitov@gmail.com>
2020-02-21 13:01:42 +0300

    abc

--- src/main/java/basix/bruceperry/FirstServlet.java
+++ src/main/java/basix/bruceperry/FirstServlet.java
@@ -17,44 +17,49 @@ doGet передает информацию в doPost
 @WebServlet("/formgetpost")
 public class FirstServlet extends HttpServlet {
 
+    private interface HTMLGenerator{
+        class Form{
+            private static String makeForm(HttpServletRequest request) {
+                return "<html><head>"
+                +"<title>Help Page</title></head><body>"
+                +"<h2>Please submit your information</h2>"
+
+                //make sure method="post" so that the servlet service method
+                //calls doPost in the response to this form submit
+                +
+                        "<form method=\"post\" action =\"" + request.getContextPath() +
+                                "/formgetpost\" >"
+                +"<table border=\"0\"><tr><td valign=\"top\">"
+                +"Your first name: </td>  <td valign=\"top\">"
+                +"<input type=\"text\" name=\"firstname\" size=\"20\">"
+                +"</td></tr><tr><td valign=\"top\">"
+                +"Your last name: </td>  <td valign=\"top\">"
+                +"<input type=\"text\" name=\"lastname\" size=\"20\">"
+                +"</td></tr><tr><td valign=\"top\">"
+                +"Your email: </td>  <td valign=\"top\">"
+                +"<input type=\"text\" name=\"email\" size=\"20\">"
+                +"</td></tr><tr><td valign=\"top\">"
+                +"<input type=\"submit\" value=\"Submit Info\"></td></tr>"
+                +"</table></form>"
+                +"</body></html>";
+            }
+        }
+    }
+
+
   public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
 
-	System.out.println("command: "+request.getParameter("command"));
+      System.out.println("command: " + request.getParameter("command"));
 
-	//set the MIME type of the response, "text/html"
-    response.setContentType("text/html");
+      //set the MIME type of the response, "text/html"
+      response.setContentType("text/html");
 
-	//use a PrintWriter send text data to the client who has requested the servlet
-    PrintWriter out = response.getWriter();
+      //use a PrintWriter send text data to the client who has requested the servlet
+      PrintWriter out = response.getWriter();
 
-	//Begin assembling the HTML content
-    out.println("<html><head>");
-
-    out.println("<title>Help Page</title></head><body>");
-    out.println("<h2>Please submit your information</h2>");
-
-   //make sure method="post" so that the servlet service method
-   //calls doPost in the response to this form submit
-    out.println(
-        "<form method=\"post\" action =\"" + request.getContextPath() +
-            "/formgetpost\" >");
-
-    out.println("<table border=\"0\"><tr><td valign=\"top\">");
-    out.println("Your first name: </td>  <td valign=\"top\">");
-    out.println("<input type=\"text\" name=\"firstname\" size=\"20\">");
-    out.println("</td></tr><tr><td valign=\"top\">");
-	out.println("Your last name: </td>  <td valign=\"top\">");
-    out.println("<input type=\"text\" name=\"lastname\" size=\"20\">");
-    out.println("</td></tr><tr><td valign=\"top\">");
-    out.println("Your email: </td>  <td valign=\"top\">");
-    out.println("<input type=\"text\" name=\"email\" size=\"20\">");
-    out.println("</td></tr><tr><td valign=\"top\">");
-
-    out.println("<input type=\"submit\" value=\"Submit Info\"></td></tr>");
-    out.println("</table></form>");
-    out.println("</body></html>");
-     } //end doGet
+      out.println(HTMLGenerator.Form.makeForm(request));
+  }
 
 	 public void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException{
@@ -76,7 +81,8 @@ public class FirstServlet extends HttpServlet {
     PrintWriter out = response.getWriter();
 
 	//Begin assembling the HTML content
-    out.println("<html><head>");
+
+         out.println("<html><head>");
     out.println("<title>Submitted Parameters</title></head><body>");
 
 	if (emptyEnum){
