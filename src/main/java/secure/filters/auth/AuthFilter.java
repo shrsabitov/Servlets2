fdb883d7468f0cc31c69483d5aa68b0aa15fef72
Shamil <sh.r.sabitov@gmail.com>
2020-04-29 13:07:42 +0300

    d

--- src/main/java/secure/filters/auth/AuthFilter.java
+++ src/main/java/secure/filters/auth/AuthFilter.java
@@ -50,7 +50,7 @@ public class AuthFilter implements Filter {
                         throw new UnsupportedOperationException("Don't call this!");
                     }
                 }
-
+*
                 И соответствующий фильтр, где с помощью обертки создаем новый риспонс. Иначе пришлось бы слишком много
                 переопределять, если напрямую HttpServletResponse response=new HttpServletResponse ... :
 
